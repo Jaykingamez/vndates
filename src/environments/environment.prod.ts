002e351494d2690259b346285a91b9c23dc24232
Jaykingamez <jaykinchan@gmail.com>
2022-01-15 22:29:07 +0800

    add production files for deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBuvFz3_TYzgs_6gHR-6zodxakp2R2PY34",
+    authDomain: "vndates-582b8.firebaseapp.com",
+    projectId: "vndates-582b8",
+    storageBucket: "vndates-582b8.appspot.com",
+    messagingSenderId: "669523751365",
+    appId: "1:669523751365:web:f5710a6bd35bdd6d7c9bac"
+  }
 };
